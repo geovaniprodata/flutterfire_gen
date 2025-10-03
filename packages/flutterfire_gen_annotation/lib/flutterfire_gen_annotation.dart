47a6d61e56cab5b217d19835aad7e26131a49f4d
Geovani Amaral <geovani.web13@gmail.com>
2025-10-03 09:30:15 -0300

    chore: update pubspec.lock versions and add ignore_json_serialization export

--- packages/flutterfire_gen_annotation/lib/flutterfire_gen_annotation.dart
+++ packages/flutterfire_gen_annotation/lib/flutterfire_gen_annotation.dart
@@ -2,4 +2,5 @@ export 'src/always_use_server_timestamp.dart';
 export 'src/default.dart';
 export 'src/field_value.dart';
 export 'src/firestore_document.dart';
+export 'src/ignore_json_serialization.dart';
 export 'src/json_post_processor.dart';
